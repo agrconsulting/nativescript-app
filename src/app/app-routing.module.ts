dad705d2e1bf5f90e1c858c05fb8669f261c18cb
agrconsulting <development@agrconsulting.co>
2019-04-20 15:20:25 -0700

    commit3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,11 @@ import { NativeScriptRouterModule } from 'nativescript-angular/router';
 import { Routes } from "@angular/router";
 
 import { AuthComponent } from "./auth/auth.component";
+import { TodayComponent } from "./challenges/today/today.component";
 
 const routes: Routes = [
     { path: '', component: AuthComponent },
-    { path: 'today' }
+    { path: 'today', component: TodayComponent }
 ];
 
 @NgModule({
