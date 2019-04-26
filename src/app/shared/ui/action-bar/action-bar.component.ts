108e97b88cc31febb102dfefa507265661fa6bdc
agrconsulting <development@agrconsulting.co>
2019-04-26 14:04:44 -0700

    sidedrawer-menu-build

--- src/app/shared/ui/action-bar/action-bar.component.ts
+++ src/app/shared/ui/action-bar/action-bar.component.ts
@@ -19,6 +19,10 @@ export class ActionBarComponent implements OnInit {
 
   ngOnInit() {}
 
+  get android() {
+      return isAndroid;
+  }
+
   get canGoBack() {
     return this.router.canGoBack() && this.showBackButton;
   }
